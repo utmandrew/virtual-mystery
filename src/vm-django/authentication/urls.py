c0a986d7f1dc1b8f864186b5807f6e4867e926cc
ibrahim <ibrahim@DESKTOP-LA96VAJ>
2018-07-06 19:57:29 -0400

    added a logout endpoint

--- src/vm-django/authentication/urls.py
+++ src/vm-django/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.conf.urls import url
 from rest_framework.authtoken import views
+from .views import Logout
 
 urlpatterns = [
     url(r'^token$', views.obtain_auth_token),
+    url(r'^logout$', Logout.as_view()),
 ]
