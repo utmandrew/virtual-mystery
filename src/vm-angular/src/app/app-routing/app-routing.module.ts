071f637b7f81b80191abd1a727d80445ce455c32
ibrahim <ibrahim@DESKTOP-LA96VAJ>
2018-07-07 09:50:34 -0400

    added an auth angular app to handle login/logout

---
+++ src/vm-angular/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from '../auth/auth.component';
+
+const routes: Routes = [
+	{ path: '', redirectTo: 'auth', pathMatch: 'full' },
+	{ path: 'auth', component: AuthComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/vm-angular/src/app/app.module.ts
+++ src/vm-angular/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
