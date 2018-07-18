ec90f46dc317c56f2c1781085731b6a5dbe95e5e
ibrahim <ibrahim@DESKTOP-LA96VAJ>
2018-07-18 16:57:26 -0400

    created serializers for system and mystery models

--- src/vm-django/mystery/serializers.py
+++ src/vm-django/mystery/serializers.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from .models import Mystery, Instance
+
+
+class MysterySerializer(serializers.ModelSerializer):
+    """
+    Serializes/Deserializes Mystery class objects
+    """
+    class Meta:
+        model = Mystery
+        fields = ('id', 'name')
+
+
+class InstanceSerializer(serializers.ModelSerializer):
+    """
+    Serializes/Deserializes Mystery class objects
+    """
+    class Meta:
+        model = Instance
+        fields = ('id', 'group', 'mystery')
