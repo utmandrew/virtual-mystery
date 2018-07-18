00f6191af3c3db629884955e2dfd3ca24d21b687
ibrahim <ibrahim@DESKTOP-LA96VAJ>
2018-07-18 16:47:19 -0400

    created system and mystery models

--- src/vm-django/mystery/models.py
+++ src/vm-django/mystery/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Mystery(models.Model):
+    """
+    Mystery template.
+    """
+    # mystery name
+    name = models.TextField()
+
+
+class Instance(models.Model):
+    """
+    Mystery instance.
+    """
+    # refers to a mystery template
+    mystery = models.ForeignKey(Mystery, related_name='instance',
+                                on_delete=models.CASCADE)
+    # refers to a group
+    group = models.ForeignKey('system.Group', related_name='instance',
+                              on_delete=models.CASCADE)
--- src/vm-django/system/models.py
+++ src/vm-django/system/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+
+class Practical(models.Model):
+    """
+    Practical model, Groups refer to a specific practical that they belong to.
+    """
+    # to be implemented
+    pass
+
+
+class Group(models.Model):
+    """
+    Group model, Users refer to a specific group that they belong to.
+    """
+    # group name
+    name = models.TextField()
+    # refers to a groups practical
+    # remove null=True when in production (used for admin accounts)
+    practical = models.ForeignKey(Practical, null=True, related_name='group',
+                                  on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    """
+    Extension of default django user model. (no inheritance)
+    User profile info.
+    """
+    # link to the default django user model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # refers to a users group
+    # remove null=True when in production (used for admin accounts)
+    group = models.ForeignKey(Group, null=True, related_name='profile',
+                                 on_delete=models.CASCADE)
+
+
+# Delete depending on whether a user should have their profile info filled out
+# on signup or not
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Creates a Profile object for each user.
+    """
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
