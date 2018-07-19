695ffab7d2a4ff10a59014cdadafeed74b7a7919
ibrahim <ibrahim@DESKTOP-LA96VAJ>
2018-07-19 16:55:47 -0400

    created comment, reply models and serializers

--- src/vm-django/comments/models.py
+++ src/vm-django/comments/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    """
+    A comment model.
+    """
+    # datetime created
+    created = models.DateTimeField(auto_now_add=True)
+    # comment owner
+    owner = models.ForeignKey('auth.User', related_name='comment',
+                              on_delete=models.CASCADE)
+    # mystery instance
+    instance = models.ForeignKey('mystery.Instance', related_name='comment',
+                                 on_delete=models.CASCADE)
+    # mystery release (clue/week)
+    # release = models.PositiveIntegerField()
+    # comment text (no max char count)
+    text = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
+
+
+class Reply(models.Model):
+    """
+    A comment reply model.
+    """
+    # datetime created
+    created = models.DateTimeField(auto_now_add=True)
+    # reply owner
+    owner = models.ForeignKey('auth.User', related_name='reply',
+                              on_delete=models.CASCADE)
+    # reply text (no max char count)
+    text = models.TextField()
+    # refers to parent comment
+    parent = models.ForeignKey(Comment, on_delete=models.CASCADE,
+                               related_name="reply")
+
+    class Meta:
+        ordering = ('created',)
--- src/vm-django/comments/serializers.py
+++ src/vm-django/comments/serializers.py
@@ -1,1 +1,29 @@
 from rest_framework import serializers
+from .models import Comment, Reply
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    """
+    Serializes/Deserializes Reply class objects.
+    """
+    username = serializers.CharField(source='owner.username')
+
+    class Meta:
+        model = Reply
+        fields = ('id', 'username', 'text')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializes/Deserializes Comment class objects.
+
+    Note:
+        - Do not need to include instance
+
+    """
+    reply = ReplySerializer(many=True, read_only=True)
+    username = serializers.CharField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'username', 'instance', 'text', 'reply')
