d8e4a7170375141b29f925df863bf029f4a49bd8
ibrahim <ibrahim@DESKTOP-LA96VAJ>
2018-07-31 22:35:59 -0400

    added release model and updated mystery model

--- src/vm-django/VM_Django/settings.py
+++ src/vm-django/VM_Django/settings.py
@@ -170,6 +170,6 @@ STATIC_URL = '/static/'
 
 DATETIME_FORMAT = "%d/%m/%Y %H:%M:%S"
 # mystery start datetime (in datetime format)
-START_DATETIME = "24/07/2018 00:00:00"
+START_DATETIME = "30/07/2018 00:00:00"
 # time interval in days
 RELEASE_INTERVAL = "7"
