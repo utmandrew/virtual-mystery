b53ed8ed5e80bdcbbe90ebf8c2c866253722fa6b
parthchampaneri <parthchampaneri@mail.utoronto.ca>
2018-08-03 11:43:57 -0400

    update to user model

--- src/vm-django/system/models.py
+++ src/vm-django/system/models.py
@@ -5,12 +5,28 @@ from django.db.models.signals import post_save
 # Create your models here.
 
 
+class Course(models.Model):
+    """
+    Course Model, practicals refer to a specific course they belong to 
+    """
+
+    name = models.TextField()
+    instructor_name = models.TextField()
+
+
+
 class Practical(models.Model):
     """
     Practical model, Groups refer to a specific practical that they belong to.
     """
     # to be implemented
-    pass
+
+    name = models.TextField()
+    ta_name = models.TextField()
+
+    course = models.ForeignKey(Practical, null=True, related_name='practical',
+                                  on_delete=models.CASCADE)
+
 
 
 class Group(models.Model):
@@ -19,17 +35,33 @@ class Group(models.Model):
     """
     # group name
     name = models.TextField()
+
     # refers to a groups practical
     # remove null=True when in production (used for admin accounts)
     practical = models.ForeignKey(Practical, null=True, related_name='group',
                                   on_delete=models.CASCADE)
 
 
-class User(AbstractUser):
-    """
-    Custom user model.
-    """
-    # refers to a users group
-    # remove null=True when in production (used for admin accounts)
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Respents a "user profile" inside our system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    is_ta = models.BooleanField(default=False)
+
     group = models.ForeignKey(Group, null=True, related_name='profile',
                               on_delete=models.CASCADE)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to a string"""
+
+        return self.name
--- src/vm-django/system/serializers.py
+++ src/vm-django/system/serializers.py
@@ -2,13 +2,23 @@ from rest_framework import serializers
 from .models import Profile, Group, Practical
 
 
+class CourseSerializer(serializers.ModelSerializer):
+    """
+    """
+    class Meta:
+        model = Course
+        fields ={'id','name','instructor_name'}
+        extra_kwargs={
+            'instructor_name'
+        }
+
 class PracticalSerializer(serializers.ModelSerializer):
     """
     Serializes/Deserializes Practical class objects
     """
     class Meta:
         model = Practical
-        fields = ('id',)
+        fields = ('id','name','ta_name')
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -25,5 +35,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     Serializes/Deserializes Profile class objects
     """
     class Meta:
-        model = Profile
-        fields = ('group',)
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password','group',)
+        extra_kwargs = {'password': {'write_only': True}}
