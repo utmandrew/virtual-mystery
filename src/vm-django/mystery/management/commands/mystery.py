88ccf91c7b4dc1aeedb7cade44ce7ecbbf17d214
ibrahim <ibrahim@DESKTOP-LA96VAJ>
2018-10-02 01:01:37 -0400

    mystery parser updated - minimal error checking

--- src/vm-django/mystery/management/commands/mystery.py
+++ src/vm-django/mystery/management/commands/mystery.py
@@ -1,5 +1,103 @@
 import os
+import re
+from shutil import copyfile
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from mystery.models import Mystery, Release
+
+
+STATIC_DIR = os.path.join("mystery","static","mystery")
+
+
+def get_release_number(rname):
+    """
+    Extracts release number from release name.
+
+    Format: Release#
+
+    :param rname: release name (string)
+    :return: release number (int)
+    """
+
+    return re.findall('^.*?(\d+)$', rname)[0]
+
+
+def create_mystery(name):
+    """
+    Creates mystery objects based on mystery folder info.
+
+    :param name: mystery name (string)
+    :return: returns mystery object hash attribute
+    """
+    mystery, _ = Mystery.objects.get_or_create(
+        name=name,
+    )
+
+    print("mystery created")
+
+    return mystery.hash
+
+
+def create_mystery_folder(mhash):
+    """
+    Creates mystery folder in django static folder.
+
+    :param mhash: mystery objects hash attribute
+    """
+    abs_path = os.path.join(settings.BASE_DIR, STATIC_DIR, mhash)
+    if not os.path.exists(abs_path):
+        os.mkdir(abs_path)
+
+
+def create_release(number, ans, clue, mname):
+    """
+    Creates release object based on release folder info.
+
+    :param number: release number (int)
+    :param ans: answer text (string)
+    :param clue: clue test (string)
+    :param mname: mystery name (string)
+    :return: (mystery hash (string), release hash (string))
+    """
+
+    mystery = Mystery.objects.get(
+        name=mname,
+    )
+
+    release, _ = Release.objects.get_or_create(
+        mystery=mystery,
+        number=number,
+        clue=clue,
+        answer=ans,
+    )
+
+    return mystery.hash, release.hash
+
+
+def create_release_folder(mhash, rhash):
+    """
+    Creates release folder in django static folder.
+    """
+    abs_path = os.path.join(settings.BASE_DIR, STATIC_DIR, mhash, rhash)
+    if not os.path.exists(abs_path):
+        os.mkdir(abs_path)
+
+
+def move_images(mhash, rhash, rpath, nimages):
+    """
+    Moves images from release folder to release hash folder in static folder.
+    :param mhash: mystery hash (string)
+    :param rhash: release hash (string)
+    :param rpath: release folder absolute path (string)
+    :param nimages: number of images in release folder (int)
+    """
+    abs_path = os.path.join(settings.BASE_DIR, STATIC_DIR, mhash, rhash)
+    if os.path.exists(abs_path):
+        while nimages != 0:
+            image = "image" + str(nimages) + ".jpg"
+            copyfile(os.path.join(rpath, image),
+                     os.path.join(abs_path, image))
+            nimages -= 1
 
 
 class Command(BaseCommand):
@@ -32,31 +130,67 @@ class Command(BaseCommand):
             if os.path.isdir(fpath):
                 # traverses files top to bottom recursively from fpath
                 for root, dirs, files in os.walk(fpath):
+                    # ans.txt text
+                    ans = ""
+                    # clue.txt test
+                    clue = ""
+                    # image counter
+                    nimages = 0
+
                     # release folder
                     if files:
-                        print(os.path.basename(root))
+                        # print(os.path.basename(root))
                         # for files in release folder
                         for file_path in files:
                             # text file
-                            if file_path.endswith("txt"):
-                                file = open(os.path.join(root, file_path), 'r')
+                            if file_path.lower().endswith("clue.txt"):
+                                # clue file
+                                with open(os.path.join(root, file_path), 'r') \
+                                        as file:
 
-                                print(file_path)
+                                    # saves clue
+                                    clue = file.read()
 
-                                # for line in file:
-                                #     print(line)
+                            elif file_path.lower().endswith("ans.txt"):
+                                # answer file
+                                with open(os.path.join(root, file_path), 'r') \
+                                        as file:
 
-                                print(file.read())
+                                    # saves answer
+                                    ans = file.read()
 
-                                file.close()
+                            elif file_path.lower().endswith("jpg"):
+                                # image file
+
+                                # increments image counter
+                                nimages += 1
+
+                        # create release object
+
+                        # release number
+                        rnumber = get_release_number(os.path.basename(root))
+                        # mystery name
+                        mname = os.path.basename(os.path.dirname(root))
+                        # create release
+                        mhash, rhash = create_release(rnumber, ans, clue, mname)
+                        # creates release static folder
+                        create_release_folder(mhash, rhash)
+                        # moves images to release static folder
+                        move_images(mhash, rhash, root, nimages)
+
                     # mystery folder
-                    elif root != path:
-                        print("Mystery " + os.path.basename(root))
+                    elif root != fpath:
+                        # creates mystery object
+                        mhash = create_mystery(os.path.basename(root))
+                        # creates mystery static folder
+                        create_mystery_folder(mhash)
             else:
                 # fpath not a path to folder
-                pass
-        except:
-            pass
+                self.stderr.write(self.style.ERROR("Provided path is not a "
+                                                   "folder."))
+        except FileNotFoundError:
+            # file path does not exist
+            self.stderr.write(self.style.ERROR("File does not exist."))
 
 
 
