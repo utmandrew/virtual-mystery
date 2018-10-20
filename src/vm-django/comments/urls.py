4c7611ad7e68f8c06e9c13daa0928ab30c1bcbb4
parthchampaneri <parthchampaneri@mail.utoronto.ca>
2018-10-20 13:53:38 -0400

    Added level of confirmation before submitting marks for a user, a Modal

--- src/vm-django/comments/urls.py
+++ src/vm-django/comments/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         name='comment_list'),
     url(r'^create$', views.CommentCreate.as_view(), name='comment_create'),
     url(r'^reply$', views.ReplyCreate.as_view(), name='reply_create'),
-    url(r'^(?P<release>[0-9]*)/(?P<groupName>.*)$', views.TaCommentList.as_view(),name='ta_comment_list'),
+    url(r'^(?P<release>[0-9]*)/(?P<groupId>.*)$', views.TaCommentList.as_view(),name='ta_comment_list'),
     url(r'^resultCreate$', views.ResultCreate.as_view(), name='result_create'),
     url(r'^userResult$', views.UserResult.as_view(), name='user_result'),
 
