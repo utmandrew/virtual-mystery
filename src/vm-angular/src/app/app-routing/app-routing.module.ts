a2669ad304997d9f9c081a049cf1f0e98cb1880f
3370sohail@gmail.com <sohail.hameed@mail.utoronto.ca>
2018-10-25 18:37:50 -0400

    added active guard to new routes

--- src/vm-angular/src/app/app-routing/app-routing.module.ts
+++ src/vm-angular/src/app/app-routing/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../auth/login/login.component';
-import { MysteryComponent } from '../mystery/mystery.component'; 
+import { MysteryComponent } from '../mystery/mystery.component';
 import { ReleaseViewComponent } from '../mystery/release-view/release-view.component';
 import { ReleaseListComponent } from '../mystery/release-list/release-list.component';
 import { NotFoundComponent } from '../not-found.component';
@@ -12,20 +12,20 @@ import { TAComponent } from '../ta/ta.component';
 const routes: Routes = [
 	{ path: '', redirectTo: 'auth', pathMatch: 'full' },
 	{ path: 'auth', component: LoginComponent },
-	{ 
+	{
 		path: 'mystery',
 		children: [
 			{ path: 'release/list', component: ReleaseListComponent },
-			
+
 			// Make sure release/:id is the last path!
 			{ path: 'release/:id', component: ReleaseViewComponent },
 		],
 		canActivate: [AuthGuardService],
 		runGuardsAndResolvers: 'always'
 	},
-	{ path: 'grades', component: GradesComponent },
+	{ path: 'grades', component: GradesComponent , canActivate: [AuthGuardService], runGuardsAndResolvers: 'always'},
 
-	{path: 'taview', component: TAComponent},
+	{path: 'taview', component: TAComponent, canActivate: [AuthGuardService], runGuardsAndResolvers: 'always'},
 
 	// Make sure ** is the last path!
 	{ path: '**', component: NotFoundComponent }
