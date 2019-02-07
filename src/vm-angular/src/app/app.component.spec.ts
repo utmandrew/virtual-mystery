3445b569c87c0ab3bf7dba6c5c33fb739003bbe8
3370sohail <3370sohail@gmail.com>
2019-02-07 01:35:16 -0500

    got it down to 17 errors

--- src/vm-angular/src/app/app.component.spec.ts
+++ src/vm-angular/src/app/app.component.spec.ts
@@ -1,11 +1,71 @@
 import { TestBed, async } from '@angular/core/testing';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import {APP_BASE_HREF} from '@angular/common';
+// custom modules
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+// custom services/interceptors
+import { AuthInterceptor } from './auth/auth.interceptor';
+// custom components
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+import { CommentComponent } from './comment/comment.component';
+import { CommentcreateComponent } from './comment/commentcreate/commentcreate.component';
+import { CommentlistComponent } from './comment/commentlist/commentlist.component';
+import { ReplycreateComponent } from './comment/commentlist/replycreate/replycreate.component';
+import { MysteryComponent } from './mystery/mystery.component';
+import { ReleaseViewComponent } from './mystery/release-view/release-view.component';
+import { NotFoundComponent } from './not-found.component';
+import { ReleaseListComponent } from './mystery/release-list/release-list.component';
+import { ArtifactViewComponent } from './artifact-view/artifact-view.component';
+import { TAComponent } from './ta/ta.component';
+
+import { ResultViewComponent } from './mystery/release-view/result-view/result-view.component';
+import { GradesComponent } from './grades/grades.component';
+import { TaInstructionsComponent } from './ta-instructions/ta-instructions.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        AuthComponent,
+        LoginComponent,
+        LogoutComponent,
+        CommentComponent,
+        CommentcreateComponent,
+        CommentlistComponent,
+        ReplycreateComponent,
+        NotFoundComponent,
+        MysteryComponent,
+      ReleaseViewComponent,
+      ReleaseListComponent,
+      ArtifactViewComponent,
+      TAComponent,
+
+      ResultViewComponent,
+
+      GradesComponent,
+
+      TaInstructionsComponent,
       ],
+      imports: [
+        BrowserModule,
+    	HttpClientModule,
+    	AppRoutingModule,
+    	FormsModule,
+    	ReactiveFormsModule
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/'}
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
