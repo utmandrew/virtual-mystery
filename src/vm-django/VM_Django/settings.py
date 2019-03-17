a0f149328be73270f98f59c5369ff94721d00971
3370sohail <3370sohail@gmail.com>
2019-03-17 15:20:15 -0400

    tesing django evn vars

--- src/vm-django/VM_Django/settings.py
+++ src/vm-django/VM_Django/settings.py
@@ -89,17 +89,29 @@ WSGI_APPLICATION = 'VM_Django.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vm_database',
-        'USER': 'vm_db_user',
-        'PASSWORD': 'vm_password',
-        'HOST': 'db',
-        'PORT': '5432',
+if 'DOCKER' in os.environ and os.environ.get('DOCKER') == 'True':
+    print("!!!!!!!!!!!!!!!!!hello environment var for docker works !!!!!!!!!!!!!!!!!")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('POSTGRES_DB'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'HOST': 'db',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'vm_database',
+            'USER': 'vm_db_user',
+            'PASSWORD': 'vm_password',
+            'HOST': 'db',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
