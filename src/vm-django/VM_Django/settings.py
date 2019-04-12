44841f19e459a983dbd445516be0fb7067f0b633
Parth-Champaneri <parth.champaneri@hotmail.com>
2019-04-12 15:23:08 -0400

    Revert back to previous commit

--- src/vm-django/VM_Django/settings.py
+++ src/vm-django/VM_Django/settings.py
@@ -97,8 +97,8 @@ if 'DOCKER' in os.environ and os.environ.get('DOCKER') == 'True':
             'NAME': os.environ.get('POSTGRES_DB'),
             'USER': os.environ.get('POSTGRES_USER'),
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': 'db',
-            'PORT': '5432',
+            'HOST': os.environ.get('POSTGRES_HOST'),
+            'PORT': os.environ.get('POSTGRES_PORT'),
         }
     }
 else:
