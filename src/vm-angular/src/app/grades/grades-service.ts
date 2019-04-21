63de44abbed43112cb25af20787deaa3c50554b4
Ibrahim-Mahjoub <39747319+Ibrahim-Mahjoub@users.noreply.github.com>
2019-04-21 16:43:19 -0400

    Update grades-service.ts

--- src/vm-angular/src/app/grades/grades-service.ts
+++ src/vm-angular/src/app/grades/grades-service.ts
@@ -6,7 +6,7 @@ import { HttpService } from '../http.service';
   providedIn: 'root'
 })
 export class GradesService {
-  API_URL = 'http://localhost:8000';
+  API_URL = 'http://django:80';
 
   constructor(private httpClient: HttpService) { }
   
