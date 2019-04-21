b242b2e0a33fc2a12808dec58b74a956768d94bf
EC2 Default User <ec2-user@ip-172-31-95-46.ec2.internal>
2019-04-21 21:39:59 +0000

    localhost to the ip of aws server

--- src/vm-angular/src/app/http.service.ts
+++ src/vm-angular/src/app/http.service.ts
@@ -12,7 +12,7 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  API_URL = 'http://localhost:80';
+  API_URL = 'http://54.88.171.179:80';
 
   // returns an HttpHeaders object with the current user's auth token
   addAuthToken(header: HttpHeaders){
